a086bdcc3c8cc67ecbaa20a22271211ffe2aad8c
victor <victorferrero1303@gmail.com>
2024-03-12 09:26:40 +0100

    commit hecho

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
   
   public static void main(String[] args) {
+    System.out.println("Victor ferrero vega");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("IES San Andres");
+    System.out.println("vivo en leon");
   }
   
 }
